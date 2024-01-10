39cd1634885cfc769659fb024b11beeb4a8b83ac
rande2 <151875901+rande2@users.noreply.github.com>
2024-01-10 13:03:27 -0500

    Add files via upload

---
+++ source/Spinner.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package randomPicker;
+import java.util.List;
+/**
+ *
+ * @author rande2
+ */
+public class Spinner {
+    private List<String> allNames;
+    private List<String> remainingNames;
+    
+    Spinner(List<String> n){
+        allNames=n;
+        remainingNames=n;
+    }
+    
+    public List<String> getRemainingNames(){
+        return remainingNames;
+    }
+    
+    public List<String> getAllNames(){
+        return allNames;
+    }
+    
+    public void setNames(List<String> n){
+        allNames=n;
+        remainingNames=n;
+    }
+    
+    public String spin(){
+        int index = (int)(Math.random()*(remainingNames.size()));
+        String name = remainingNames.get(index);
+        remainingNames.remove(index);
+        return name;
+    }
+}
