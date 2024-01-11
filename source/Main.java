89d5781544d47c64cd92746c7a42fe96bb58e66b
rande2 <151875901+rande2@users.noreply.github.com>
2024-01-11 14:52:04 -0500

    Add files via upload

--- source/Main.java
+++ source/Main.java
@@ -4,6 +4,8 @@
  */
 package randomPicker;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.Arrays;
@@ -37,14 +39,40 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    private static final String fileNames = "files.txt";
+ 
     public static void main(String[] args) {
-        //list with letters
+        IO.init();
+        Scanner in = new Scanner(System.in);
+        
+        File file = IO.getFile(fileNames,Location.PROGRAM);
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+            }catch(IOException ex){
+                System.err.println(ex);
+            }
+        }
+        
+        LinkedList<String> files = IO.readLines(
+                IO.getInputStream(fileNames,Location.PROGRAM)
+        );
+        
+        
+        
         LinkedList<String> students = new LinkedList<String>(Arrays.asList("a","b","c","d","e","f"));
         
-        Scanner in = new Scanner(System.in);
         Spinner spinner = new Spinner(students);
         String name;
         
+        
+        
+        while((name=spinner.spin())!=null){
+            System.out.println(name);
+            System.out.println("Press enter to continue");
+            in.nextLine();
+        }
+        spinner.reset();
         while((name=spinner.spin())!=null){
             System.out.println(name);
             System.out.println("Press enter to continue");
