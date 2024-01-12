cc0f2e67126c7494e41dd1c364cd21f0736a041e
mohamakdm <135855442+mohamakdm@users.noreply.github.com>
2024-01-12 13:28:15 -0500

    Create Student.java
    Student Object

---
+++ source/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+
+
+    String firstName;
+    String lastName;
+
+    public Student(String fn, String ln){
+
+        firstName = fn;
+        lastName = ln;
+
+    }
+
+    public String getFirstName(){
+        return(firstName);
+    }
+    public String getLastName(){
+        return(lastName);
+    }
+}
