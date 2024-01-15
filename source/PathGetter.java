a3b7e7242221f9abf04e5ea98105ddc70c1badc9
rande2 <151875901+rande2@users.noreply.github.com>
2024-01-15 13:43:41 -0500

    Add files via upload

--- source/PathGetter.java
+++ source/PathGetter.java
@@ -2,11 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package fileio;
+package bankProject;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,37 +25,11 @@ public class PathGetter {
     private static String findProgramDir() {
         //get path to the JAR file or build folder
         String dir = new File(System.getProperty("java.class.path")).getAbsolutePath();
-        //check if the location is to a JAR file
-        //if so, remove the jar file's name from the path
-        String jar = ".jar";
-        int jarLength = jar.length();
-        int progDirLen = dir.length();
-        int lengthDiff = progDirLen - jarLength;
-        boolean isJar = true;
-        //check of the path ends with ".jar"
-        for (int i = 0; i < jarLength; i++) {
-            if (jar.charAt(i) != (dir.charAt(i + lengthDiff))) {
-                isJar = false;
-                break;
-            }
-        }
-        //remove the JAR file from the path
-        if (isJar) {
-            String temp = dir;
-            for (int i = progDirLen - 5; i > 0; i--) {
-                if (dir.charAt(i) == System.getProperty("file.separator").charAt(0)) {
-                    dir = dir.substring(0, i + 1);
-                    break;
-                }
-            }
-            //directory is [name].jar
-            if (dir.equals(temp)) {
-                dir = "";
-            }
-        } else {
-            dir += System.getProperty("file.separator");
-        }
-        return dir;
+        Path path = Paths.get(dir);
+        if(!Files.isDirectory(path))
+            return path.getParent().toString()+System.getProperty("file.separator");
+        return path.toString();
+        
     }
 
     private static String findUserHome() {
@@ -69,6 +47,16 @@ public class PathGetter {
     public static Path getPath(String name, Location l) {
         return Paths.get((l == Location.PROGRAM ? PROGRAM_DIR : USER_HOME) + name);
     }
-
     
+    public static void ensureExistance(Path path){
+        if(!Files.exists(path))
+            try {
+                Path parent = path.getParent();
+                if(!Files.exists(parent))
+                    Files.createDirectories(parent);
+                Files.createFile(path);
+            } catch (IOException ex) {
+                Logger.getLogger(PathGetter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
 }
