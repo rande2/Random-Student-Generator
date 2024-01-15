1c2cd676ad7871da29b45865dc4fc8e22217d584
mohamakdm <135855442+mohamakdm@users.noreply.github.com>
2024-01-15 14:55:16 -0500

    Update Main(student sorter test).java

--- source/Main(student sorter test).java
+++ source/Main(student sorter test).java
@@ -11,21 +11,36 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        ArrayList<Student> students =
+                new ArrayList<>(30);
+
+
         Student st1 = new Student("Mohamad", "Halal");
-        Student st2 = new Student("Mohamad", "Hall");
+        Student st2 = new Student("Mohamad", "H");
         Student st3 = new Student("Rowan", "A");
         Student st4 = new Student("Ing", "O");
-        Scanner in = new Scanner(System.in);
-        LinkedList<Student> students =
-                new LinkedList<Student>(Arrays.asList(st1,st2,st3,st4));
+
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
 
         sort(students);
 
-        System.out.println("What would you like to search for?");
+
+        printList(students);
+
+        System.out.println("First name");
 
         String searchTerm = in.next();
 
-        int result = binarySearch(students, searchTerm);
+
+            System.out.println("Last Name");
+        String ln = in.next();
+
+        int result = binarySearch(students, searchTerm,ln);
 
         if(result == -1){
             System.out.println("Name not found");
@@ -36,18 +51,21 @@ public class Main {
 
     }
 
-     public static int binarySearch(LinkedList<Student> ll, String x  ){
+     public static int binarySearch(ArrayList<Student> ll, String x, String ln){
          int l = 0, r = ll.size() - 1;
          while(l<=r){
              int m = l + (r-1)/2;
-             int res = x.compareTo(ll.get(m).getFirstName());
+
+             int resLn = ln.compareTo(ll.get(m).getLastName());
+             int resFn = x.compareTo(ll.get(m).getFirstName());
+
 
              // Check if x is present at mid
-             if (res == 0)
+             if (resLn == 0 && resFn == 0)
                  return m;
 
              // If x greater, ignore left half
-             if (res > 0)
+             if (resLn > 0 && resFn > 0)
                  l = m + 1;
 
                  // If x is smaller, ignore right half
@@ -57,12 +75,21 @@ public class Main {
 
          return -1;
      }
-    public static void sort(LinkedList<Student> list) {
+    public static void sort(ArrayList<Student> list) {
 
         list.sort((o1, o2)
                 -> o1.getFirstName().compareTo(
                 o2.getFirstName()));
     }
+
+
+    public static void printList(ArrayList<Student> list){
+        for(Student student: list){
+            System.out.println(student.getFirstName() + "  " + student.getLastName());
+        }
+    }
+
+
 }
 
 
